1676dc1c44fb418ec409715919bdf15939b8c126
beata <beata@emc2wave.com>
2019-11-20 18:21:15 -0800

    create contact State

--- app.js
+++ app.js
@@ -30,4 +30,22 @@ const aboutState = function(page) {
   document.querySelector('#content').innerHTML = `
     <p>This is the about page</p>
   `
-}
+};
+
+// Contact State
+const contactState = function(page) {
+  document.querySelector('#heading').textContent = 'Contact Us';
+  document.querySelector('#content').innerHTML = `
+    <form>
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" class="form-control">
+      </div>
+      <div class="form-group">
+      <label>Email address</label>
+      <input type="email" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  `
+};
